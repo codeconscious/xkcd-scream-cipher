6363e6e7aa3fefff5db0e7b3ca8163cf1764556f
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-04-18 15:32:05 +0900

    Code improvements

--- src/Encoding.fs
+++ src/Encoding.fs
@@ -4,45 +4,45 @@ open System
 open System.Globalization
 open Utilities
 
-let private encodingPairs =
+let private encodingMap =
     [ ("A", "A"); ("B", "Ȧ"); ("C", "A̧"); ("D", "A̠"); ("E", "Á"); ("F", "A̮")
       ("G", "A̋"); ("H", "A̰"); ("I", "Ả"); ("J", "A̓"); ("K", "Ạ"); ("L", "Ă")
       ("M", "Ǎ"); ("N", "Â"); ("O", "Å"); ("P", "A̯"); ("Q", "A̤"); ("R", "Ȃ")
       ("S", "Ã"); ("T", "Ā"); ("U", "Ä"); ("V", "À"); ("W", "Ȁ"); ("X", "A̽")
       ("Y", "A̦"); ("Z", "A̷") ]
+    |> Map.ofList
 
-let encode (unencoded: string) =
-    let convert ch =
-        let asStr = ch.ToString()
-        let encodingMap = Map.ofList encodingPairs
+let private decodingMap =
+    // For detecting some additional characters that might be used instead of the expected ones.
+    let extraPairs =
+        [ ("A̸", "Z"); ("A̅", "T"); ("Ȧ", "B"); ("Á", "E"); ("Ả", "I"); ("Ạ", "K")
+          ("Ă", "L"); ("Ǎ", "M"); ("Â", "N"); ("Å", "O"); ("Ȃ", "R"); ("Ã", "S")
+          ("Ā", "T"); ("Ä", "U"); ("À", "V"); ("Ȁ", "W"); ("A̱", "D"); ("A̲", "D") ]
+        |> Map.ofList
 
-        match encodingMap.TryGetValue asStr with
-        | true, found -> found
-        | false, _ -> asStr
+    encodingMap
+    |> flipMap
+    |> combineMaps extraPairs
 
-    unencoded
+let encode (unencodedText: string) =
+    let convert unencoded =
+        let unencodedAsStr = unencoded.ToString()
+
+        match encodingMap.TryGetValue unencodedAsStr with
+        | true, encoded -> encoded
+        | false, _ -> unencodedAsStr
+
+    unencodedText
     |> Seq.map convert
     |> String.concat String.Empty
 
-let decode (encoded: string) =
-    let decodingMap =
-        // For detecting some additional characters that might be used instead of the expected ones.
-        let extraDecodingPairs =
-            [ ("A̸", "Z"); ("A̅", "T"); ("Ȧ", "B"); ("Á", "E"); ("Ả", "I"); ("Ạ", "K")
-              ("Ă", "L"); ("Ǎ", "M"); ("Â", "N"); ("Å", "O"); ("Ȃ", "R"); ("Ã", "S")
-              ("Ā", "T"); ("Ä", "U"); ("À", "V"); ("Ȁ", "W"); ("A̱", "D"); ("A̲", "D") ]
+let decode (encodedText: string) =
+    let convert encoded =
+        match decodingMap.TryGetValue encoded with
+        | true, decoded -> decoded
+        | false, _ -> encoded
 
-        encodingPairs
-        |> List.map flip // Arrange encoded chars as keys.
-        |> List.append extraDecodingPairs
-        |> Map.ofList
-
-    let convert text =
-        match decodingMap.TryGetValue text with
-        | true, decodedText -> decodedText
-        | false, _ -> text
-
-    let encodedStringInfo = StringInfo encoded
+    let encodedStringInfo = StringInfo encodedText
 
     // `SubstringByTextElements` is used to properly iterate over composed Unicode characters.
     let extractCharAt i : string = encodedStringInfo.SubstringByTextElements(i, 1)
--- src/Utilities.fs
+++ src/Utilities.fs
@@ -2,7 +2,11 @@ module Utilities
 
 open System
 
-let flip (x, y) = y, x
+let flipMap map : Map<'a, 'b> =
+      Map.fold (fun m k v -> m.Add(v, k)) Map.empty map
+
+let combineMaps map1 map2 :  Map<'a,'b> =
+    Map.fold (fun acc key value -> Map.add key value acc) map1 map2
 
 let caseInsensitiveEquals (x: string) (y: string) : bool =
     x.Equals(y, StringComparison.InvariantCultureIgnoreCase)
