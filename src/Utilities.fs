94b249a82ac850db343ec18012660ec8002033b7
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-04-19 19:07:18 +0900

    Preserve order of flags using a Dictionary

--- src/Utilities.fs
+++ src/Utilities.fs
@@ -12,3 +12,10 @@ let merge secondary primary : Map<'a, 'b> =
 let caseInsensitiveEquals (x: string) (y: string) : bool =
     x.Equals(y, StringComparison.InvariantCultureIgnoreCase)
 
+let groupByValues items : seq<'a * 'b list> =
+    items
+    |> Seq.map (fun (KeyValue(k, vs)) -> vs, k)
+    |> Seq.groupBy fst
+    |> Seq.map (fun (op, pairs) ->
+        let keys = pairs |> Seq.map snd |> Seq.toList
+        op, keys)
