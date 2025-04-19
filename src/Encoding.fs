5833f7a9e502f19adf5f87d9f352dfea931f7be6
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-04-19 10:53:10 +0900

    Encoding code cleanup (#5)

--- src/Encoding.fs
+++ src/Encoding.fs
@@ -4,48 +4,48 @@ open System
 open System.Globalization
 open Utilities
 
-let private encodingPairs =
+let private encodingMap =
     [ ("A", "A"); ("B", "Ȧ"); ("C", "A̧"); ("D", "A̠"); ("E", "Á"); ("F", "A̮")
       ("G", "A̋"); ("H", "A̰"); ("I", "Ả"); ("J", "A̓"); ("K", "Ạ"); ("L", "Ă")
       ("M", "Ǎ"); ("N", "Â"); ("O", "Å"); ("P", "A̯"); ("Q", "A̤"); ("R", "Ȃ")
       ("S", "Ã"); ("T", "Ā"); ("U", "Ä"); ("V", "À"); ("W", "Ȁ"); ("X", "A̽")
       ("Y", "A̦"); ("Z", "A̷") ]
+    |> Map.ofList
 
-let encode (unencoded: string) =
-    let convert ch =
-        let asStr = ch.ToString()
-        let encodingMap = Map.ofList encodingPairs
-
-        match encodingMap.TryGetValue asStr with
-        | true, found -> found
-        | false, _ -> asStr
-
-    unencoded
-    |> Seq.map convert
-    |> String.concat String.Empty
-
-let decode (encoded: string) =
-    let decodingMap =
-        // For detecting some additional characters that might be used instead of the expected ones.
-        let extraDecodingPairs =
-            [ ("A̸", "Z"); ("A̅", "T"); ("Ȧ", "B"); ("Á", "E"); ("Ả", "I"); ("Ạ", "K")
-              ("Ă", "L"); ("Ǎ", "M"); ("Â", "N"); ("Å", "O"); ("Ȃ", "R"); ("Ã", "S")
-              ("Ā", "T"); ("Ä", "U"); ("À", "V"); ("Ȁ", "W"); ("A̱", "D"); ("A̲", "D") ]
-
-        encodingPairs
-        |> List.map flip // Arrange encoded chars as keys.
-        |> List.append extraDecodingPairs
+let private decodingMap =
+    // For detecting some additional characters that might be used instead of the expected ones.
+    let extraPairs =
+        [ ("A̸", "Z"); ("A̅", "T"); ("Ȧ", "B"); ("Á", "E"); ("Ả", "I"); ("Ạ", "K")
+          ("Ă", "L"); ("Ǎ", "M"); ("Â", "N"); ("Å", "O"); ("Ȃ", "R"); ("Ã", "S")
+          ("Ā", "T"); ("Ä", "U"); ("À", "V"); ("Ȁ", "W"); ("A̱", "D"); ("A̲", "D") ]
         |> Map.ofList
 
-    let convert text =
-        match decodingMap.TryGetValue text with
-        | true, decodedText -> decodedText
-        | false, _ -> text
+    encodingMap
+    |> flip
+    |> merge extraPairs
 
-    let encodedStringInfo = StringInfo encoded
+let encode (input: string) =
+    let convert input =
+        let inputAsStr = input.ToString()
+
+        match encodingMap.TryGetValue inputAsStr with
+        | true, encoded -> encoded
+        | false, _ -> inputAsStr
+
+    input
+    |> Seq.map convert
+    |> String.Concat
+
+let decode (encodedText: string) =
+    let convert encoded =
+        match decodingMap.TryGetValue encoded with
+        | true, decoded -> decoded
+        | false, _ -> encoded
+
+    let encodedStringInfo = StringInfo encodedText
 
     // `SubstringByTextElements` is used to properly iterate over composed Unicode characters.
-    let extractCharAt i : string = encodedStringInfo.SubstringByTextElements(i, 1)
+    let extractCharAt i = encodedStringInfo.SubstringByTextElements(i, 1)
 
     [| 0 .. encodedStringInfo.LengthInTextElements - 1 |]
     |> Array.map extractCharAt
@@ -53,12 +53,12 @@ let decode (encoded: string) =
     |> String.Concat
 
 // Confirms that provided input is correctly encoded and decoded to its original value.
-let test (unencodedText: string) =
-    let encoded = encode unencodedText
+let test (input: string) =
+    let encoded = encode input
     let decoded = decode encoded
     let result =
-        if caseInsensitiveEquals unencodedText decoded
+        if caseInsensitiveEquals input decoded
         then "OK"
         else "ERROR"
 
-    $"%s{result}: %s{unencodedText} --> %s{encoded} --> %s{decoded}"
+    $"%s{result}: %s{input} --> %s{encoded} --> %s{decoded}"
--- src/Utilities.fs
+++ src/Utilities.fs
@@ -2,7 +2,13 @@ module Utilities
 
 open System
 
-let flip (x, y) = y, x
+let flip map : Map<'a, 'b> =
+    Map.fold (fun acc k v -> Map.add v k acc) Map.empty map
+
+// Merge two maps with the values of the 'primary' one taking precedence.
+let merge secondary primary : Map<'a, 'b> =
+    Map.fold (fun acc k v -> Map.add k v acc) secondary primary
 
 let caseInsensitiveEquals (x: string) (y: string) : bool =
     x.Equals(y, StringComparison.InvariantCultureIgnoreCase)
+
